571dc91976dc2c4daf88081f03b50521486a1d94
sodawater <soda95@qq.com>
2017-10-22 13:07:34 +0800

    modify data_utils

--- data_utils.py
+++ data_utils.py
@@ -63,6 +63,49 @@ def create_vocabulary(vocabulary_path, data_path, max_vocabulary_size,
           #print(w + "\n")
           vocab_file.write(w + "\n")
 
+def create_vocabulary_samevocab(from_vocab_path, to_vocab_path, from_data_path, to_data_path, max_vocabulary_size,
+                      tokenizer=None, normalize_digits=False):
+  if not gfile.Exists(from_vocab_path):
+    print("Creating vocabulary %s from data %s" % (from_vocab_path, from_data_path))
+    vocab = {}
+    with gfile.GFile(from_data_path, mode="r") as f:
+      counter = 0
+      for line in f:
+        counter += 1
+        if counter % 100000 == 0:
+          print("  processing line %d" % counter)
+        tokens = tokenizer(line) if tokenizer else basic_tokenizer(line)
+        for w in tokens:
+          word = _DIGIT_RE.sub(r"0", w) if normalize_digits else w
+          if word in vocab:
+            vocab[word] += 1
+          else:
+            vocab[word] = 1
+    with gfile.GFile(to_data_path, mode="r") as f:
+          counter = 0
+          for line in f:
+              counter += 1
+              if counter % 100000 == 0:
+                  print("  processing line %d" % counter)
+              tokens = tokenizer(line) if tokenizer else basic_tokenizer(line)
+              for w in tokens:
+                  word = _DIGIT_RE.sub(r"0", w) if normalize_digits else w
+                  if word in vocab:
+                      vocab[word] += 1
+                  else:
+                      vocab[word] = 1
+    vocab_list = _START_VOCAB + sorted(vocab, key=vocab.get, reverse=True)
+    if len(vocab_list) > max_vocabulary_size:
+        vocab_list = vocab_list[:max_vocabulary_size]
+    with gfile.GFile(from_vocab_path, mode="w") as vocab_file:
+        for w in vocab_list:
+            # print(w + "\n")
+            vocab_file.write(w + "\n")
+    with gfile.GFile(to_vocab_path, mode="w") as vocab_file:
+        for w in vocab_list:
+            # print(w + "\n")
+            vocab_file.write(w + "\n")
+
 
 def initialize_vocabulary(vocabulary_path):
   if gfile.Exists(vocabulary_path):
@@ -104,24 +147,24 @@ def data_to_token_ids(data_path, target_path, vocabulary_path,
           tokens_file.write(" ".join([str(tok) for tok in token_ids]) + "\n")
 
 def prepare_data(data_dir, from_train_path, to_train_path, from_dev_path, to_dev_path, from_vocabulary_size,
-                 to_vocabulary_size, tokenizer=None):
+                 to_vocabulary_size, same_vocab=False, tokenizer=None):
+    to_vocab_path = os.path.join(data_dir, "vocab%d.to" % to_vocabulary_size)
+    from_vocab_path = os.path.join(data_dir, "vocab%d.from" % from_vocabulary_size)
+    to_train_ids_path = to_train_path + (".ids%d" % to_vocabulary_size)
+    from_train_ids_path = from_train_path + (".ids%d" % from_vocabulary_size)
+    to_dev_ids_path = to_dev_path + (".ids%d" % to_vocabulary_size)
+    from_dev_ids_path = from_dev_path + (".ids%d" % from_vocabulary_size)
+    if same_vocab == False:
+        create_vocabulary(to_vocab_path, to_train_path, to_vocabulary_size, tokenizer)
+        create_vocabulary(from_vocab_path, from_train_path, from_vocabulary_size, tokenizer)
+    else:
+        create_vocabulary_samevocab(from_vocab_path, to_vocab_path, from_train_path, to_train_path, from_vocabulary_size, tokenizer)
+    data_to_token_ids(to_train_path, to_train_ids_path, to_vocab_path, tokenizer)
+    data_to_token_ids(from_train_path, from_train_ids_path, from_vocab_path, tokenizer)
 
-  to_vocab_path = os.path.join(data_dir, "vocab%d.to" % to_vocabulary_size)
-  from_vocab_path = os.path.join(data_dir, "vocab%d.from" % from_vocabulary_size)
-  create_vocabulary(to_vocab_path, to_train_path , to_vocabulary_size, tokenizer)
-  create_vocabulary(from_vocab_path, from_train_path , from_vocabulary_size, tokenizer)
+    data_to_token_ids(to_dev_path, to_dev_ids_path, to_vocab_path, tokenizer)
+    data_to_token_ids(from_dev_path, from_dev_ids_path, from_vocab_path, tokenizer)
 
-  to_train_ids_path = to_train_path + (".ids%d" % to_vocabulary_size)
-  from_train_ids_path = from_train_path + (".ids%d" % from_vocabulary_size)
-  data_to_token_ids(to_train_path, to_train_ids_path, to_vocab_path, tokenizer)
-  data_to_token_ids(from_train_path, from_train_ids_path, from_vocab_path, tokenizer)
-
-
-  to_dev_ids_path = to_dev_path + (".ids%d" % to_vocabulary_size)
-  from_dev_ids_path = from_dev_path + (".ids%d" % from_vocabulary_size)
-  data_to_token_ids(to_dev_path, to_dev_ids_path, to_vocab_path, tokenizer)
-  data_to_token_ids(from_dev_path, from_dev_ids_path, from_vocab_path, tokenizer)
-
-  return (from_train_ids_path, to_train_ids_path,
+    return (from_train_ids_path, to_train_ids_path,
           from_dev_ids_path, to_dev_ids_path,
           from_vocab_path, to_vocab_path)

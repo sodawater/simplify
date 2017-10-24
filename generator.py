eb42d4d2160625e98aca1e8321cb72f842fb2417
sodawater <soda95@qq.com>
2017-10-24 11:23:32 +0800

    modify generator

--- generator.py
+++ generator.py
@@ -4,187 +4,99 @@ import numpy as np
 import data_utils
 import random
 import os
+from tensorflow.python.layers import core as layers_core
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 class Generator():
-    def __init__(self, num_emb, batch_size, emb_dim, hidden_dim, max_seqlen, start_token, learning_rate=0.01, reward_gamma=0.95):
-        self.num_emb = num_emb
-        self.batch_size = batch_size
-        self.emb_dim = emb_dim
-        self.hidden_dim = hidden_dim
-        self.sequence_length = max_seqlen
-        self.start_token = tf.constant([start_token] * batch_size, dtype=tf.int32)
-        self.learning_rate = tf.Variable(float(learning_rate), trainable=False)
-        self.reward_gamma = reward_gamma
-        self.g_params = []
-        self.d_params = []
-        self.temperature = 1.0
-        self.grad_clip = 5.0
-        self.expected_reward = tf.Variable(tf.zeros([self.sequence_length]))
+    def __init__(self,hparams, mode):
+        self.vocab_size = hparams.to_vocab_size
+        self.emb_dim = hparams.emb_dim
+        self.num_units = hparams.units
+        self.num_layers = hparams.num_layers
+        self.learning_rate = tf.Variable(float(hparams.learning_rate), trainable=False)
+        self.clip_value = hparams.clip_value
+        self.max_seq_length = 50
+        self.learning_rate_decay_op = self.learning_rate.assign(self.learning_rate * hparams.decay_factor)
 
-        with tf.variable_scope('generator'):
-            self.g_embeddings = tf.Variable(self.init_matrix([self.num_emb, self.emb_dim]))
-            self.g_params.append(self.g_embeddings)
-            self.g_recurrent_unit = self.create_recurrent_unit(self.g_params)
-            self.g_output_unit = self.create_output_unit(self.g_params)
+        if mode != tf.contrib.learn.ModeKeys.INFER:
+            self.decoder_input_ids = tf.placeholder(dtype=tf.int32, shape=[None,None])
+            self.decoder_input_length = tf.placeholder(dtype=tf.int32, shape=[None])
+            self.initial_state = tf.placeholder(dtype=tf.float32, shape=[None, None, None])
+            self.batch_size = tf.size(self.decoder_input_length)
+        else:
+            self.batch_size = 1
 
-        self.x = tf.placeholder(tf.int32, shape=[self.batch_size,
-                                                 None])
-        self.rewards = tf.placeholder(tf.float32, shape=[self.batch_size,
-                                                         None])
-        self.target_weights = tf.placeholder(tf.float32, shape=[self.batch_size,
-                                                                None])
-        self.output_length = tf.placeholder(tf.int32)
+        with tf.variable_scope("embedding") as scope:
+            self.embeddings = tf.Variable(self.init_matrix([self.vocab_size, self.emb_dim]))
 
-        with tf.device("/cpu:0"):
-            self.processed_x = tf.transpose(tf.nn.embedding_lookup(self.g_embeddings, self.x),
-                                            perm=[1, 0, 2])  # seq_length x batch_size x emb_dim
+        with tf.variable_scope("projection") as scope:
+            self.output_layer = layers_core.Dense(self.vocab_size)
 
-        self.z = tf.placeholder(tf.float32, [self.batch_size, self.hidden_dim])
-        self.h0 = tf.stack([self.z, self.z])
-        gen_o = tf.TensorArray(dtype=tf.float32, size=self.sequence_length, dynamic_size=True)
-        gen_x = tf.TensorArray(dtype=tf.int32, size=self.sequence_length, dynamic_size=True)
+        with tf.variable_scope("decoder") as scope:
+            if self.num_layers > 1:
+                decoder_cell = tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.BasicLSTMCell(self.num_units) for _ in range(self.num_layers)])
+            else:
+                decoder_cell = tf.contrib.rnn.BasicLSTMCell(self.num_units)
+            if mode != tf.contrib.learn.ModeKeys.INFER:
+                initial_state = self.initial_state
+                with tf.device("/cpu:0"):
+                    decoder_inputs = tf.nn.embedding_lookup(self.embeddings, self.decoder_input_ids)
+                helper = tf.contrib.seq2seq.TrainingHelper(decoder_inputs, self.decoder_input_length)
+                my_decoder = tf.contrib.seq2seq.BasicDecoder(cell=decoder_cell,
+                                                             helper=helper,
+                                                             initial_state=initial_state,
+                                                             output_layer=self.output_layer
+                                                             )
+                decoder_outputs, decoder_state, decoder_output_len = tf.contrib.seq2seq.dynamic_decode(my_decoder,
+                                                                                                       maximum_iterations=self.max_seq_length * 2,
+                                                                                                       swap_memory=True,)
+                self.sample_id = decoder_outputs.sample_id
+                self.logits = decoder_outputs.rnn_output
+            else:
+                helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(self.embeddings, [hparams.GO_ID], hparams.EOS_ID)
+                initial_state = self.initial_state
+                my_decoder = tf.contrib.seq2seq.BasicDecoder(cell=decoder_cell,
+                                                             helper=helper,
+                                                             initial_state=initial_state,
+                                                             output_layer=self.output_layer)
+                decoder_outputs, decoder_state, decoder_output_len = tf.contrib.seq2seq.dynamic_decode(my_decoder,
+                                                                                                       maximum_iterations=self.max_seq_length * 2,
+                                                                                                       swap_memory=True)
+                self.sample_id = tf.unstack(decoder_outputs.sample_id, axis=0)
 
-        def _g_recurrence(i, x_t, h_tm1, gen_o, gen_x):
-            h_t = self.g_recurrent_unit(x_t, h_tm1)  # hidden_memory_tuple
-            o_t = self.g_output_unit(h_t)  # batch x vocab , logits not prob
-            log_prob = tf.log(tf.nn.softmax(o_t))
-            next_token = tf.cast(tf.reshape(tf.multinomial(log_prob, 1), [self.batch_size]), tf.int32)
-            x_tp1 = tf.nn.embedding_lookup(self.g_embeddings, next_token)  # batch x emb_dim
-            gen_o = gen_o.write(i, tf.reduce_sum(tf.multiply(tf.one_hot(next_token, self.num_emb, 1.0, 0.0),
-                                                             tf.nn.softmax(o_t)), 1))  # [batch_size] , prob
-            gen_x = gen_x.write(i, next_token)  # indices, batch_size
-            return i + 1, x_tp1, h_t, gen_o, gen_x
+        if mode != tf.contrib.learn.ModeKeys.INFER:
+            self.targets = tf.placeholder(dtype=tf.int32, shape=[None, None])
+            self.target_weights = tf.placeholder(dtype=tf.float32, shape=[None, None])
+            self.rewards = tf.placeholder(dtype=tf.float32, shape=[None, None])
+            with tf.variable_scope("loss") as scope:
+                crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.targets, logits=self.logits)
+                self.pretrain_loss = tf.reduce_sum(crossent * self.target_weights) / tf.to_float(self.batch_size)
+                self.train_loss = tf.reduce_sum(crossent * self.rewards) / tf.to_float(self.batch_size)
 
-        _, _, _, self.gen_o, self.gen_x = tf.while_loop(
-            cond=lambda i, _1, _2, _3, _4: i < self.output_length,
-            body=_g_recurrence,
-            loop_vars=(tf.constant(0, dtype=tf.int32),
-                       tf.nn.embedding_lookup(self.g_embeddings, self.start_token), self.h0, gen_o, gen_x))
-        self.gen_x = self.gen_x.stack()  # seq_length x batch_size
-        self.gen_x = tf.transpose(self.gen_x, perm=[1, 0])  # batch_size x seq_length
+            if mode == tf.contrib.learn.ModeKeys.TRAIN:
+                self.pretrain_global_step = tf.Variable(0, trainable=False)
+                self.train_global_step = tf.Variable(0, trainable=False)
+                with tf.variable_scope("train_op") as scope:
+                    optimizer = tf.train.AdamOptimizer(self.learning_rate)
 
+                    pretrain_gradients, pretrain_v = zip(*optimizer.compute_gradients(self.pretrain_loss))
+                    pretrain_gradients, _ = tf.clip_by_global_norm(pretrain_gradients, self.clip_value)
+                    self.pretrain_train_op = optimizer.apply_gradients(zip(pretrain_gradients, pretrain_v),
+                                                              global_step=self.pretrain_global_step)
 
-        g_predictions = tf.TensorArray(
-            dtype=tf.float32, size=5, dynamic_size=True)
-        g_outputs = tf.TensorArray(dtype=tf.int32, size=5, dynamic_size=True)
-
-        ta_emb_x = tf.TensorArray(
-            dtype=tf.float32, size=self.sequence_length, dynamic_size=True, name="ta_emb_x")
-        ta_emb_x = ta_emb_x.unstack(self.processed_x)
-        #self.ta_emb_x = ta_emb_x
-
-
-        def _pretrain_recurrence(i, x_t, h_tm1, g_predictions, g_outputs):
-            h_t = self.g_recurrent_unit(x_t, h_tm1)
-            o_t = self.g_output_unit(h_t)
-            g_predictions = g_predictions.write(i, tf.nn.softmax(o_t))  # batch x vocab_size
-            log_prob = tf.log(tf.nn.softmax(o_t))
-            next_token = tf.cast(tf.reshape(tf.multinomial(log_prob, 1), [self.batch_size]), tf.int32)
-            g_outputs = g_outputs.write(i, next_token)
-            x_tp1 = ta_emb_x.read(i)
-            return i + 1, x_tp1, h_t, g_predictions, g_outputs
-
-        _, _, _, self.g_predictions, self.g_outputs = tf.while_loop(
-            cond=lambda i, _1, _2, _3, _4: i < self.output_length,
-            body=_pretrain_recurrence,
-            loop_vars=(tf.constant(0, dtype=tf.int32),
-                       tf.nn.embedding_lookup(self.g_embeddings, self.start_token),
-                       self.h0, g_predictions, g_outputs))
-
-
-        self.g_predictions = tf.transpose(self.g_predictions.stack(),
-                                          perm=[1, 0, 2])  # batch_size x seq_length x vocab_size
-        self.g_outputs = tf.transpose(self.g_outputs.stack(), perm=[1,0])
-
-        print(tf.reshape(self.target_weights, [-1]))
-        print(self.rewards)
-        # pretraining loss
-        """
-        self.pretrain_loss = -tf.reduce_sum(
-            tf.one_hot(tf.to_int32(tf.reshape(self.x, [-1])), self.num_emb, 1.0, 0.0) * tf.log(
-                tf.clip_by_value(tf.reshape(self.g_predictions, [-1, self.num_emb]), 1e-20, 1.0)
-            ), 1
-        ) * tf.reshape(self.target_weights, [-1]) / (self.output_length * self.batch_size)
-        """
-        self.pretrain_loss = -tf.reduce_sum(
-            tf.reduce_sum(
-                tf.one_hot(tf.to_int32(tf.reshape(self.x, [-1])), self.num_emb, 1.0, 0.0) * tf.log(
-                    tf.clip_by_value(tf.reshape(self.g_predictions, [-1, self.num_emb]), 1e-20, 1.0)
-                ), 1) * tf.reshape(self.target_weights, [-1])
-        )
-        # training updates
-        pretrain_opt = self.g_optimizer(self.learning_rate)
-
-        self.pretrain_grad, _ = tf.clip_by_global_norm(tf.gradients(self.pretrain_loss, self.g_params), self.grad_clip)
-        self.pretrain_updates = pretrain_opt.apply_gradients(zip(self.pretrain_grad, self.g_params))
-
-        #######################################################################################################
-        #  Unsupervised Training
-        #######################################################################################################
-        self.g_loss = -tf.reduce_sum(
-            tf.reduce_sum(
-                tf.one_hot(tf.to_int32(tf.reshape(self.x, [-1])), self.num_emb, 1.0, 0.0) * tf.log(
-                    tf.clip_by_value(tf.reshape(self.g_predictions, [-1, self.num_emb]), 1e-20, 1.0)
-                ), 1) * tf.reshape(self.rewards, [-1])
-        )
-
-        g_opt = self.g_optimizer(self.learning_rate)
-
-        self.g_grad, _ = tf.clip_by_global_norm(tf.gradients(self.g_loss, self.g_params), self.grad_clip)
-        self.g_updates = g_opt.apply_gradients(zip(self.g_grad, self.g_params))
+                    train_gradients, train_v = zip(*optimizer.compute_gradients(self.train_loss))
+                    train_gradients, _ = tf.clip_by_global_norm(train_gradients, self.clip_value)
+                    self.train_op = optimizer.apply_gradients(zip(train_gradients, train_v),
+                                                              global_step=self.train_global_step)
 
         self.saver = tf.train.Saver(tf.global_variables())
 
-    def get_batch_simple(self, data, buckets, bucket_id):
-        decoder_size = buckets[bucket_id]
-        decoder_inputs = []
-        batch_weights = []
-        # Get a random batch of encoder and decoder inputs from data,
-        # pad them if needed, reverse encoder inputs and add GO to decoder.
-        for _ in range(self.batch_size):
-            decoder_input = random.choice(data[bucket_id])
-
-            # Decoder inputs get an extra "GO" symbol, and are padded then.
-            decoder_pad_size = decoder_size - len(decoder_input)
-            decoder_inputs.append(decoder_input +
-                                  [data_utils.PAD_ID] * decoder_pad_size)
-            batch_weights.append([1.0] * (len(decoder_input)) + [0.0] * decoder_pad_size)
-
-        # Now we create batch-major vectors from the data selected above.
-        """
-        batch_decoder_inputs, batch_weights = [], []
-        # Batch decoder inputs are re-indexed decoder_inputs, we create weights.
-        for length_idx in range(decoder_size):
-            batch_decoder_inputs.append(
-                np.array([decoder_inputs[batch_idx][length_idx]
-                          for batch_idx in range(self.batch_size)], dtype=np.int32))
-
-            # Create target_weights to be 0 for targets that are padding.
-            batch_weight = np.ones(self.batch_size, dtype=np.float32)
-            for batch_idx in range(self.batch_size):
-                # We set weight to 0 if the corresponding target is a PAD symbol.
-                # The corresponding target is decoder_input shifted by 1 forward.
-                if length_idx < decoder_size - 1:
-                    target = decoder_inputs[batch_idx][length_idx + 1]
-                if length_idx == decoder_size - 1 or target == data_utils.PAD_ID:
-                    batch_weight[batch_idx] = 0.0
-            batch_weights.append(batch_weight)
-        return batch_decoder_inputs, batch_weights
-        """
-        return decoder_inputs, batch_weights
-    def get_train_batch(self, data, buckets, bucket_id):
-        return
-
-    def generate(self, sess, z, output_length):
-        outputs = sess.run(self.gen_x,
-                           feed_dic={self.z:z,
-                                     self.output_length:output_length})
+    def generate(self, sess, initial_state, output_length):
+        outputs = sess.run(self.sample_id,
+                           feed_dic={self.initial_state:initial_state})
         return outputs
 
     def pretrain_step(self, sess, x, z, target_weights, output_length):
-        #print(output_length)
-        #print(x)
         outputs = sess.run([self.pretrain_updates, self.pretrain_loss, self.g_outputs],
                            feed_dict={self.x: x,
                                       self.z:z,
@@ -196,83 +108,3 @@ class Generator():
 
     def init_matrix(self, shape):
         return tf.random_normal(shape, stddev=0.1)
-
-    def init_vector(self, shape):
-        return tf.zeros(shape)
-
-    def create_recurrent_unit(self, params):
-        # Weights and Bias for input and hidden tensor
-        self.Wi = tf.Variable(self.init_matrix([self.emb_dim, self.hidden_dim]))
-        self.Ui = tf.Variable(self.init_matrix([self.hidden_dim, self.hidden_dim]))
-        self.bi = tf.Variable(self.init_matrix([self.hidden_dim]))
-
-        self.Wf = tf.Variable(self.init_matrix([self.emb_dim, self.hidden_dim]))
-        self.Uf = tf.Variable(self.init_matrix([self.hidden_dim, self.hidden_dim]))
-        self.bf = tf.Variable(self.init_matrix([self.hidden_dim]))
-
-        self.Wog = tf.Variable(self.init_matrix([self.emb_dim, self.hidden_dim]))
-        self.Uog = tf.Variable(self.init_matrix([self.hidden_dim, self.hidden_dim]))
-        self.bog = tf.Variable(self.init_matrix([self.hidden_dim]))
-
-        self.Wc = tf.Variable(self.init_matrix([self.emb_dim, self.hidden_dim]))
-        self.Uc = tf.Variable(self.init_matrix([self.hidden_dim, self.hidden_dim]))
-        self.bc = tf.Variable(self.init_matrix([self.hidden_dim]))
-        params.extend([
-            self.Wi, self.Ui, self.bi,
-            self.Wf, self.Uf, self.bf,
-            self.Wog, self.Uog, self.bog,
-            self.Wc, self.Uc, self.bc])
-
-        def unit(x, hidden_memory_tm1):
-            previous_hidden_state, c_prev = tf.unstack(hidden_memory_tm1)
-
-            # Input Gate
-            i = tf.sigmoid(
-                tf.matmul(x, self.Wi) +
-                tf.matmul(previous_hidden_state, self.Ui) + self.bi
-            )
-
-            # Forget Gate
-            f = tf.sigmoid(
-                tf.matmul(x, self.Wf) +
-                tf.matmul(previous_hidden_state, self.Uf) + self.bf
-            )
-
-            # Output Gate
-            o = tf.sigmoid(
-                tf.matmul(x, self.Wog) +
-                tf.matmul(previous_hidden_state, self.Uog) + self.bog
-            )
-
-            # New Memory Cell
-            c_ = tf.nn.tanh(
-                tf.matmul(x, self.Wc) +
-                tf.matmul(previous_hidden_state, self.Uc) + self.bc
-            )
-
-            # Final Memory cell
-            c = f * c_prev + i * c_
-
-            # Current Hidden state
-            current_hidden_state = o * tf.nn.tanh(c)
-
-            return tf.stack([current_hidden_state, c])
-
-        return unit
-
-    def create_output_unit(self, params):
-        self.Wo = tf.Variable(self.init_matrix([self.hidden_dim, self.num_emb]))
-        self.bo = tf.Variable(self.init_matrix([self.num_emb]))
-        params.extend([self.Wo, self.bo])
-
-        def unit(hidden_memory_tuple):
-            hidden_state, c_prev = tf.unstack(hidden_memory_tuple)
-            # hidden_state : batch x hidden_dim
-            logits = tf.matmul(hidden_state, self.Wo) + self.bo
-            # output = tf.nn.softmax(logits)
-            return logits
-
-        return unit
-
-    def g_optimizer(self, *args, **kwargs):
-        return tf.train.AdamOptimizer(*args, **kwargs)

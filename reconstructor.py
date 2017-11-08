f205f9a94bcfe5baf1aeddde4d2e369d016d84b4
sodawater <soda95@qq.com>
2017-11-08 14:52:49 +0800

    modify

--- reconstructor.py
+++ reconstructor.py
@@ -8,6 +8,7 @@ from tensorflow.python.util import nest
 
 class Reconstructor():
     def __init__(self, hparams, mode):
+        self.hparams = hparams
         self.from_vocab_size = hparams.from_vocab_size
         self.to_vocab_size = hparams.to_vocab_size
         self.num_units = hparams.num_units
@@ -97,7 +98,7 @@ class Reconstructor():
                                                                alignment_history=True,
                                                                attention_layer_size=self.num_units)
             #########train without targets ###########
-            helper_t = tf.contrib.seq2seq.GreedyEmbeddingHelper(self.to_embeddings, tf.tile([hparams.GO_ID], [self.batch_size]), hparams.EOS_ID)
+            helper_t = tf.contrib.seq2seq.SampleEmbeddingHelper(self.to_embeddings, tf.tile([hparams.GO_ID], [self.batch_size]), hparams.EOS_ID)
             initial_state_t = decoder_cell.zero_state(self.batch_size, tf.float32).clone(cell_state=initial_state)
             my_decoder_t = tf.contrib.seq2seq.BasicDecoder(cell=decoder_cell,
                                                          helper=helper_t,
@@ -107,6 +108,7 @@ class Reconstructor():
                                                                                                                     decoder_cell.state_size.cell_state,
                                                                                                    maximum_iterations=self.max_seq_length * 2,
                                                                                                    swap_memory=True)
+            self.sample_id_t = decoder_outputs_t.sample_id
             #########################################
 
             if mode != tf.contrib.learn.ModeKeys.INFER:
@@ -132,6 +134,41 @@ class Reconstructor():
                 self.a = alignment_history
                 self.alignment_history_t = tf.unstack(tf.argmax(alignment_history, axis=2), axis=1)
                 self.sample_id_t = decoder_outputs_t.sample_id
+
+        with tf.variable_scope("rolllout") as scope:
+            if mode == tf.contrib.learn.ModeKeys.TRAIN:
+                self.rollout_input_ids = tf.placeholder(dtype=tf.int32, shape=[None, None])
+                self.rollout_input_length = tf.placeholder(dtype=tf.int32, shape=[None])
+                self.rollout_next_id = tf.placeholder(dtype=tf.int32, shape=[None])
+                with tf.device("/cpu:0"):
+                    rollout_inputs = tf.nn.embedding_lookup(self.to_embeddings, self.rollout_input_ids)
+                rollout_cell = decoder_cell
+                memory_rollout = tf.concat([encoder_outputs_fw, encoder_outputs_bw], axis=2)
+                initial_state_ro = rollout_cell.zero_sate(self.batch_size, tf.float32).clone(cell_state=encoder_state)
+                attention_mechanism_ro = tf.contrib.seq2seq.LuongAttention(num_units=self.num_units,
+                                                                           memory=memory_rollout,
+                                                                           scale=True,
+                                                                           memory_sequence_length=self.encoder_input_length)
+
+                helper_ro = tf.contrib.seq2seq.TrainingHelper(rollout_inputs, self.rollout_input_length)
+                rollout_decoder = tf.contrib.seq2seq.BasicDecoder(cell=rollout_cell,
+                                                                  helper=helper_ro,
+                                                                  initial_state=initial_state_ro,
+                                                                  output_layer=self.output_layer)
+                _, final_state_ro, _ = tf.contrib.seq2seq.dynamic_decode(rollout_decoder,
+                                                                         maximum_iterations=self.max_seq_length,
+                                                                         swap_memory=True)
+
+                initial_state_MC = decoder_cell.zero_state(self.batch_size, tf.float32).clone(cell_state=final_state_ro.cell_state)
+                helper_MC = tf.contrib.seq2seq.SampleEmbeddingHelper(self.to_embeddings, self.rollout_next_id, hparams.EOS_ID)
+                rollout_decoder_MC = tf.contrib.seq2seq.BasicDecoder(cell=rollout_cell,
+                                                                     helper=helper_MC,
+                                                                     initial_state=initial_state_MC,
+                                                                     output_layer=self.output_layer)
+                decoder_output_MC, _, _ = tf.contrib.seq2seq.dynamic_decode(rollout_decoder_MC,
+                                                                            maximum_iterations=self.max_seq_length,
+                                                                            swap_memory=True)
+                self.sample_id_MC = decoder_output_MC.sample_id
 
         with tf.variable_scope("reconstructor") as scope:
             if self.num_layers > 1:
@@ -220,10 +257,14 @@ class Reconstructor():
             self.decoder_target_weights = tf.placeholder(dtype=tf.float32, shape=[None, None])
             self.reconstructor_targets = tf.placeholder(dtype=tf.int32, shape=[None, None])
             self.reconstructor_target_weights = tf.placeholder(dtype=tf.float32, shape=[None, None])
-
+            self.rewards = tf.placeholder(dtype=tf.float32, shape=[None, None])
             with tf.variable_scope("loss") as scope:
                 crossent_t = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.reconstructor_targets, logits=self.reconstructor_logits_t)
                 self.loss_t = tf.reduce_sum(crossent_t * self.reconstructor_target_weights) / tf.to_float(self.batch_size)
+
+                crossent_ro = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.decoder_targets, logits=self.decoder_logits_pt)
+                self.loss_ro = (tf.reduce_sum(crossent_t * self.reconstructor_target_weights) +
+                                tf.reduce_sum(crossent_ro * self.rewards)) / tf.to_float(self.batch_size)
 
                 decoder_crossent_pt = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.decoder_targets, logits=self.decoder_logits_pt)
                 reconstructor_crossent_pt = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.reconstructor_targets, logits=self.reconstructor_logits_pt)
@@ -233,6 +274,7 @@ class Reconstructor():
             if mode == tf.contrib.learn.ModeKeys.TRAIN:
                 self.global_step_t = tf.Variable(0, trainable=False)
                 self.global_step_pt = tf.Variable(0, trainable=False)
+                self.global_step_ro = tf.Variable(0, trainable=False)
                 with tf.variable_scope("train_op") as scope:
                     optimizer_t = tf.train.AdamOptimizer(self.learning_rate)
                     gradients_t, v_t = zip(*optimizer_t.compute_gradients(self.loss_t))
@@ -243,10 +285,16 @@ class Reconstructor():
                     gradients_pt, v_pt = zip(*optimizer_pt.compute_gradients(self.loss_pt))
                     gradients_pt, _ = tf.clip_by_global_norm(gradients_pt, self.clip_value)
 
+                    optimizer_ro = tf.train.GradientDescentOptimizer(self.learning_rate)
+                    gradients_ro, v_ro = zip(*optimizer_pt.compute_gradients(self.loss_ro))
+                    gradients_ro, _ = tf.clip_by_global_norm(gradients_ro, self.clip_value)
+
                     self.train_op_t = optimizer_t.apply_gradients(zip(gradients_t, v_t),
                                                                   global_step=self.global_step_t)
                     self.train_op_pt = optimizer_pt.apply_gradients(zip(gradients_pt, v_pt),
                                                               global_step=self.global_step_pt)
+                    self.train_op_ro = optimizer_pt.apply_gradients(zip(gradients_ro, v_ro),
+                                                                    global_step=self.global_step_ro)
 
 
 
@@ -295,5 +343,74 @@ class Reconstructor():
         #print(len(decoder_inputs[0]), len(decoder_targets[0]))
         return encoder_in, decoder_in, reconstructor_in
 
+    def train_step(self):
+        return
+
+    def adversarial_train_step(self, sess_r, sess_d, data, buckets, bucket_id, batch_size, discriminator, rollout_num):
+        encoder_in, decoder_in, reconstructor_in = self.get_batch(self, data, buckets, bucket_id, batch_size)
+        encoder_inputs, encoder_sequence_length = encoder_in
+        decoder_inputs, decoder_targets, decoder_target_weights, decoder_sequence_length = decoder_in
+        reconstructor_inputs, reconstructor_targets, reconstructor_target_weights, recon_sequence_length = reconstructor_in
+        feed = {self.encoder_input_ids: encoder_inputs,
+                self.encoder_input_length: encoder_sequence_length}
+        samples = sess_r.run([self.sample_id_t], feed_dict=feed)
+        rewards = self.get_reward(sess_r, sess_d, encoder_in, samples, discriminator, rollout_num)
+
+    def get_reward(self, sess_r, sess_d, encoder_in, samples, discriminator, rollout_num):
+        encoder_inputs, encoder_sequence_length = encoder_in
+        sample_length = []
+        max_sample_length = 0
+        samples_list = []
+        for batch in range(len(encoder_sequence_length)):
+            sample = samples[batch].tolist()
+            if self.hparams.EOS_ID in sample:
+                sample = sample[:sample.index(self.hparams.EOS_ID) + 1]
+            length  = len(sample)
+            sample_length.append(length)
+            samples_list.append(sample)
+            if length > max_sample_length:
+                max_sample_length = length
+
+        decoder_targets = []
+        decoder_inputs = []
+        decoder_target_weights = []
+        decoder_sequence_length = sample_length
+        for batch in range(len(encoder_sequence_length)):
+            length = sample_length[batch]
+            pad_size = max_sample_length - length
+            sample = samples_list[batch]
+            input = [self.hparams.GO_ID] + sample[0:length - 1] + pad_size * [self.hparams.PAD_ID]
+            target = sample + pad_size * [self.hparams.PAD_ID]
+            weight = [1.0] * length + [0.0] * self.hparams.PAD_ID
+            decoder_inputs.append(input)
+            decoder_targets.append(target)
+            decoder_target_weights.append(weight)
+        decoder_inputs = np.array(decoder_inputs)
+        rewards = []
+        for give_num in range(1, max_sample_length):
+            feed = {self.rollout_input_ids:decoder_inputs[:,0:give_num],
+                    self.rollout_input_length:tf.tile([give_num], [self.batch_size]),
+                    self.rollout_next_id:decoder_inputs[:,give_num]}
+            for i in range(rollout_num):
+                rollout_part = sess_r.run(self.sample_id_MC, feed_dict=feed)
+                rollout_samples, origin_length = self.padding(np.concatenate([decoder_inputs[:,0:give_num], rollout_part], axis=1))
+                feed_d = {discriminator.encoder_input_ids:rollout_samples,
+                          discriminator.encoder_input_length:origin_length}
+                ypred_for_auc = sess_d.run(discriminator.ypred_for_auc, feed_dict=feed_d)
+                ypred = np.array(item[1] for item in ypred_for_auc)
+                if i == 0:
+                    rewards.append(ypred)
+                else:
+                    rewards[give_num - 1] += ypred
+        feed_d = {discriminator.encoder_input_ids:decoder_inputs,
+                  discriminator.encoder_input_length:decoder_sequence_length}
+        ypred_for_auc = sess_d.run(discriminator.ypred_for_auc, feed_dict=feed_d)
+        ypred = np.array(item[1] for item in ypred_for_auc)
+        rewards.append(ypred * rollout_num)
+        rewards = np.transpose(np.array(rewards)) / (1.0 * rollout_num)
+        return rewards
+    
+    def padding(self, max_length=0):
+        return
     #def decode(self, sess, encoder_inputs):
 
